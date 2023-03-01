65955518b2f9b2891fae7b8c037bc1da7854a09b
Gisela <angart13@hotmail.com>
2023-02-28 21:37:24 -0300

    cambiando color con js

---
+++ color.js
@@ -0,0 +1,19 @@
+
+document.getElementById('boton_color').addEventListener('click', function(){
+    document.getElementById('barra').style.backgroundColor = '#101652';
+   document.getElementById('inicio').style.backgroundColor = '#b1ccc4';
+   document.getElementById('costado').style.backgroundColor = '#4e5861';
+   document.body.style.backgroundColor = '#fff5d6';
+   document.getElementById('aboutme').style.backgroundColor = '#e1e6d3';
+   document.getElementById('descripcion').style.backgroundColor = '#e1e6d3';
+       
+});
+
+document.getElementById('boton_color2').addEventListener('click', function(){
+   document.body.style.backgroundColor = 'white';
+   document.getElementById('inicio').style.backgroundColor = '#87555c';
+   document.getElementById('costado').style.backgroundColor = '#492d49';
+   document.getElementById('aboutme').style.backgroundColor = 'white';
+   document.getElementById('descripcion').style.backgroundColor = '#d1b68d';
+   
+});
